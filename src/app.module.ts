a2ac2618655b1d2fa527307dc36d34e82a59b6df
Juan-Camilo-Sanchez-Echeverri <juancamilosanche65@gmail.com>
2026-01-19 18:37:26 -0500

    feat: add throttling support and parse MongoDB ID pipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,32 @@
+import { APP_GUARD, APP_PIPE } from '@nestjs/core';
+
 import { Module } from '@nestjs/common';
 
 import { MongooseModule } from '@nestjs/mongoose';
 
+import { ThrottlerModule, ThrottlerGuard, seconds } from '@nestjs/throttler';
+
 import { MongooseConfigService } from '@configs';
+
+import { ParseMongoIdPipe } from '@common/pipes';
 
 @Module({
   imports: [
     // Módulos comunes globales
     MongooseModule.forRootAsync({ useClass: MongooseConfigService }),
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          limit: 50,
+          ttl: seconds(60),
+        },
+      ],
+      errorMessage: 'Too many requests, please try again later.',
+    }),
+  ],
+  providers: [
+    { provide: APP_GUARD, useClass: ThrottlerGuard },
+    { provide: APP_PIPE, useClass: ParseMongoIdPipe },
   ],
 })
 export class AppModule {}
